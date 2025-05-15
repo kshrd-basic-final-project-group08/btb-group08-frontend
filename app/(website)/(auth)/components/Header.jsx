ba2346a0704c4798aa6ae0d18a23e7760eb01cc8
pichet <good100vichet@gmail.com>
2025-05-15 16:36:48 +0700

    Config fonts

--- app/(website)/(auth)/components/Header.jsx
+++ app/(website)/(auth)/components/Header.jsx
@@ -1,12 +1,14 @@
-import { SwitchLanguage } from '@/components/feature/website/button/SwitchLanguage'
-import Image from 'next/image'
+'use client'
+
 import React from 'react'
+import Image from 'next/image'
+import { SwitchLanguage } from '@/components/feature/website/button/SwitchLanguage'
 
 export const HeaderLayout = () => {
     return (
         <div className="fixed top-0 left-0 w-full z-10">
             <div className="px-5 pt-2 flex w-full justify-between items-center">
-                <Image src="/logo.png" className="h-[80px] w-[80px] object-cover" width={80} height={80} alt="Logo" />
+                <Image src="/logo.png" width={79} height={92} alt="Logo" />
                 <SwitchLanguage />
             </div>
         </div>
--- components/feature/website/button/SwitchLanguage.jsx
+++ components/feature/website/button/SwitchLanguage.jsx
@@ -17,7 +17,7 @@ export const SwitchLanguage = () => {
                     <p className="text-gray-400">{langange}</p>
                     <ArrowDown2 size={21} className="stroke-gray-400" />
                 </div>
-                <div className="bg-white shadow-2xl rounded h-0 overflow-hidden translate-y-3 group-hover:translate-y-0 transition-all group-hover:h-fit flex absolute bg-red-500 top-full left-0 flex flex-col w-full">
+                <div className="bg-white shadow-2xl rounded h-0 overflow-hidden translate-y-3 group-hover:translate-y-0 transition-all group-hover:h-fit absolute top-full left-0 flex flex-col w-full">
                     <button onClick={() => { switchLang("kh") }} type="button" className="cursor-pointer flex items-center gap-2 p-2 h-full w-full hover:bg-gray-200">
                         <Image src="/flag/khmer.svg" width={18} height={12} alt="Khmer" />
                         <span className="">Khmer</span>
